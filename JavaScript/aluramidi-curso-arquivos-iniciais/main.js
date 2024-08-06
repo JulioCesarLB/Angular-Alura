4412d729321c0d4b704cd4b68289bd0cdffea074
Julio Lopes <julio.lopes@tokiomarine.com.br>
2024-08-06 13:54:07 -0300

    finalizando curso de JS

--- JavaScript/aluramidi-curso-arquivos-iniciais/main.js
+++ JavaScript/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,13 +1,23 @@
-document.querySelector('.tecla_pom').onclick = ()=>{
-    document.querySelector('#som_tecla_pom').play();
-}
-
-function clap(){
-    document.querySelector("#som_tecla_clap").play();
-}
-const sons = document.querySelectorAll("");
 document.querySelectorAll('.tecla').forEach(botao=>{
-    botao.onclick = ()=>{
-
+    botao.onclick = function(){
+        document.querySelector(`#som_${botao.classList[1]}`)?document.querySelector(`#som_${botao.classList[1]}`).play():alert("elemento não encontrado");
     }
-})
+    
+    botao.onkeydown = function(evento){
+        if(evento.code==="Space" || evento.code==="Enter"){
+            botao.classList.add('ativa');   
+        }
+    }
+
+    botao.onkeyup = function(){
+        botao.classList.remove('ativa');
+    }
+});
+
+/*
+document.querySelectorAll('input[type=button]').forEach(input=>{
+  input.onclick = function(){
+    document.querySelector('input[type=tel]').value= document.querySelector('input[type=tel]').value+ input.value;
+  }
+});
+*/
