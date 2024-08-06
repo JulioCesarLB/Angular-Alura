628a9ab79baee6ba1a3823c941250e0e81cd6beb
Julio Cesar <julio.lifeintech@gmail.com>
2024-08-05 22:10:59 -0300

    aula js

---
+++ JavaScript/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,13 @@
+document.querySelector('.tecla_pom').onclick = ()=>{
+    document.querySelector('#som_tecla_pom').play();
+}
+
+function clap(){
+    document.querySelector("#som_tecla_clap").play();
+}
+const sons = document.querySelectorAll("");
+document.querySelectorAll('.tecla').forEach(botao=>{
+    botao.onclick = ()=>{
+
+    }
+})
